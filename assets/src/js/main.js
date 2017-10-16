aad2870353384bcdd28912ed822e3368338ca503
itchief <alex.malcev1980@gmail.com>
2017-10-16 21:49:23 +1000

    initial release

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,7 +1,10 @@
+// Импортируем jQuery
 //= ../bower_components/jquery/dist/jquery.js
 
+// Импортируем Popper
 //= ../bower_components/popper.js/dist/umd/popper.js
 
+// Импортируем необходимые js-файлы Bootstrap 4
 //= ../bower_components/bootstrap/js/dist/util.js
 //= ../bower_components/bootstrap/js/dist/alert.js
 //= ../bower_components/bootstrap/js/dist/button.js
@@ -13,3 +16,6 @@
 //= ../bower_components/bootstrap/js/dist/tab.js
 //= ../bower_components/bootstrap/js/dist/tooltip.js
 //= ../bower_components/bootstrap/js/dist/popover.js
+
+// Импортируем другие js-файлы
+//= my.js
