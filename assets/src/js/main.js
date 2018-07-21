1a60721e4520493c2dc4826735e9068d34e26852
itchief <alex.malcev1980@gmail.com>
2018-07-21 14:44:26 +1000

    Update path in main.js and main.scss

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,21 +1,21 @@
 // Импортируем jQuery
-//= ../bower_components/jquery/dist/jquery.js
+//= ../../../node_modules/jquery/dist/jquery.js
 
 // Импортируем Popper
-//= ../bower_components/popper.js/dist/umd/popper.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
 
 // Импортируем необходимые js-файлы Bootstrap 4
-//= ../bower_components/bootstrap/js/dist/util.js
-//= ../bower_components/bootstrap/js/dist/alert.js
-//= ../bower_components/bootstrap/js/dist/button.js
-//= ../bower_components/bootstrap/js/dist/carousel.js
-//= ../bower_components/bootstrap/js/dist/collapse.js
-//= ../bower_components/bootstrap/js/dist/dropdown.js
-//= ../bower_components/bootstrap/js/dist/modal.js
-//= ../bower_components/bootstrap/js/dist/scrollspy.js
-//= ../bower_components/bootstrap/js/dist/tab.js
-//= ../bower_components/bootstrap/js/dist/tooltip.js
-//= ../bower_components/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/alert.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
 
 // Импортируем другие js-файлы
 //= my.js
